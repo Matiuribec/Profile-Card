729bbeccd0a76a3fd4c77d1e658b47243cb74d1a
Matiuribec <matiuribec2020@gmail.com>
2022-11-28 23:06:35 +0000

    nuevo

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,10 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
@@ -46,11 +21,17 @@ function render(variables = {}) {
 function getSocial(variables) {
   let SocialList = "";
   let SocialNetworks = ["twitter", "github", "linkedin", "instagram"];
-  SocialNetworks.forEach(Social => {
+  SocialNetworks.forEach(function callback(Social, index) {
     if (variables[Social]) {
       SocialList =
         SocialList +
-        '<li><a href="https://${variable[Social]}.com/${variable[Social]}><i class="bi bi-${variable[Social]}"></i></a></li>';
+        '<li><a href="https://' +
+        variables[Social] +
+        ".com/" +
+        variables[Social] +
+        '"><i class="bi bi-' +
+        SocialNetworks[index] +
+        '"></i></a></li>';
     }
   });
   return SocialList;
@@ -58,18 +39,15 @@ function getSocial(variables) {
 function placeholder(text) {
   return `<span class="placeholder">${text}</span>`;
 }
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
+
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should
     includeCover: true,
-    // this is the url of the image that will used as background for the profile cover
+
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
+
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
+
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
